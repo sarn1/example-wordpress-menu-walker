3136d5acd07d7313c07b27ddd0bb1af2e91ad228
Webvolution <spham@webvolutionchicago.com>
2016-03-17 22:01:05 -0500

    version 2.1 add-ons

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <?php get_header(); ?>
-<div id="content_body">
-<section>
+<main role="main">
 <?php if (have_posts()) :
-		while (have_posts()) : the_post(); ?>
+  while (have_posts()) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <article><?php the_content(); ?></article>
+  <?php get_sidebar(); ?>
+  <?php endwhile;
 
-		<h1><?php the_title(); ?></h1>
-		<article><?php the_content(); ?></article>
+  elseif (is_404()) : ?>
 
-        <?php get_sidebar(); ?>
+  <article>
+    <h1>We're sorry...</h1>
+    <p>Looks like we can't find the page you are looking for!</p>
+  </article>
 
-	<?php endwhile;
-	elseif (is_404()) : ?>
-
-		<article>
-		<h1>We're sorry...</h1>
-		<p>Looks like we can't find the page you are looking for!</p>
-		</article>
-
-	<?php endif; ?>
-</section>
-</div>
+  <?php endif; ?>
+</main>
 <?php get_footer(); ?>
