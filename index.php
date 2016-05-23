6d45a8cc0f393b8f0a442c5bd8b5df10292ed205
Webvolution <spham@webvolutionchicago.com>
2016-05-23 13:31:56 -0500

    bug in scripts.min.js & 404 as partials

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php get_header(); ?>
 <main role="main">
-<?php if (have_posts()) :
-  while (have_posts()) : the_post(); ?>
-    <article>
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-    </article>
-    <aside>
-      <?php get_sidebar(); ?>
-    </aside>
-  <?php endwhile;
-
-  elseif (is_404()) : ?>
-
-  <article>
-    <h1>We're sorry...</h1>
-    <p>Looks like we can't find the page you are looking for!</p>
-  </article>
-
-  <?php endif; ?>
+  <?php
+  if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+      <article>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+      </article>
+      <aside>
+        <?php get_sidebar(); ?>
+      </aside>
+  <?php
+    endwhile;
+  elseif (is_404()) :
+      get_template_part('partials','404');
+  endif;
+  ?>
 </main>
 <?php get_footer(); ?>
---
+++ partials-404.php
@@ -0,0 +1,4 @@
+<article>
+  <h1>We're sorry...</h1>
+  <p>Looks like we can't find the page you are looking for!</p>
+</article>
